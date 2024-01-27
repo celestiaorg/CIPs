07704e61b28176a4b98e49d94ff830c2b1da01c2
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2024-01-26 19:04:40 -0500

    feat: init vocs

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ Read [CIP-1](https://github.com/celestiaorg/CIPs/blob/main/cips/cip-1.md) for in
 | 3 | January 24, 2024 | [Agenda](https://github.com/celestiaorg/CIPs/issues/40) | [Tweet Thread](https://x.com/JoshCStein/status/1750187535911837848?s=20) | [Recording](https://www.youtube.com/watch?v=g_8e3h6iixM) |
 | 2 | December 13, 2023 | [Agenda](https://github.com/celestiaorg/CIPs/issues/22) | [Tweet Thread](https://x.com/JoshCStein/status/1734967567075168697) | [Recording](https://www.youtube.com/watch?v=yYt600kvf4g) |
 | 1 | November 29, 2023 | [Agenda](https://github.com/celestiaorg/CIPs/issues/8)  | [Tweet Thread](https://x.com/JoshCStein/status/1729893879191621702) | [Recording](https://www.youtube.com/watch?v=EhWHHmPo_5Q) |
+
+## Site
+
+This is a [Vocs](https://vocs.dev) project bootstrapped with the Vocs CLI.
+
+│  Next steps:
+│
+◇  1. cd ./site - Navigate to project
+│
+◇  2. pnpm install - Install dependencies
+│
+◇  3. pnpm dev - Start dev server
+│
+◇  4. Head to http://localhost:5173
---
+++ site/vocs.config.ts
@@ -0,0 +1,71 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'CIPs',
+  sidebar: [
+    {
+      text: 'CIP-1',
+      link: '/cips/cip-1',
+    },
+    {
+      text: 'CIP-2',
+      link: '/cips/cip-2',
+    },
+    {
+      text: 'CIP-3',
+      link: '/cips/cip-3',
+    },
+    {
+      text: 'CIP-4',
+      link: '/cips/cip-4',
+    },
+    {
+      text: 'CIP-5',
+      link: '/cips/cip-5',
+    },
+    {
+      text: 'CIP-6',
+      link: '/cips/cip-6',
+    },
+    {
+      text: 'CIP-7',
+      link: '/cips/cip-7',
+    },
+    {
+      text: 'CIP-8',
+      link: '/cips/cip-8',
+    },
+    {
+      text: 'CIP-9',
+      link: '/cips/cip-9',
+    },
+    {
+      text: 'CIP-10',
+      link: '/cips/cip-10',
+    },
+    {
+      text: 'CIP-11',
+      link: '/cips/cip-11',
+    },
+    {
+      text: 'CIP-12',
+      link: '/cips/cip-12',
+    },
+    {
+      text: 'CIP-13',
+      link: '/cips/cip-13',
+    },
+    {
+      text: 'CIP-14',
+      link: '/cips/cip-14',
+    },
+    {
+      text: 'CIP-15',
+      link: '/cips/cip-15',
+    },
+    {
+      text: 'CIP Template',
+      link: '/cips/cip-template',
+    }
+  ],
+})
