2c49ffbb5f60651d69fb643712fc64c9cef88ae8
jcstein <mail@jcstein.dev>
2024-01-29 20:53:12 -0500

    feat: fix frontmatter workflow

--- site/addFrontmatter.js
+++ site/addFrontmatter.js
@@ -6,18 +6,27 @@ function addFrontmatterToContent(filePath) {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const parsedContent = matter(fileContent);
 
-  // Check if frontmatter already exists in the markdown body
+  // Convert the Date object to a string in the yyyy-mm-dd format
+  const createdDate = new Date(parsedContent.data.created);
+  const createdDateString = `${createdDate.getFullYear()}-${String(createdDate.getMonth() + 1).padStart(2, '0')}-${String(createdDate.getDate()).padStart(2, '0')}`;
+
+  // Replace the Date object with the formatted string
+  parsedContent.data.created = createdDateString;
+
+  // Check if frontmatter already exists in the markdown body and at the top of the file
   const frontmatterInBody = parsedContent.content.includes(`**CIP**: ${parsedContent.data.cip}`) &&
                             parsedContent.content.includes(`**Author**: ${parsedContent.data.author}`) &&
                             parsedContent.content.includes(`**Status**: ${parsedContent.data.status}`) &&
                             parsedContent.content.includes(`**Type**: ${parsedContent.data.type}`) &&
                             parsedContent.content.includes(`**Created**: ${parsedContent.data.created}`);
-
-  if (frontmatterInBody) {
-    return; // If frontmatter already exists in the markdown body, return without writing the file
-  }
   
-  // If frontmatter doesn't exist in the markdown body, add it
+  const frontmatterAtTop = fileContent.startsWith(`---\ncip: ${parsedContent.data.cip}\ntitle: ${parsedContent.data.title}\nauthor: ${parsedContent.data.author}\nstatus: ${parsedContent.data.status}\ntype: ${parsedContent.data.type}\ncreated: ${parsedContent.data.created}\n---`);
+
+  if (frontmatterInBody && frontmatterAtTop) {
+    return; // If frontmatter already exists in the markdown body and at the top of the file, return without writing the file
+  }
+
+  // If frontmatter doesn't exist in the markdown body or at the top of the file, add it
   let newContent = `# ${parsedContent.data.title}\n\n`;
   newContent += `**CIP**: ${parsedContent.data.cip}\n\n`;
   newContent += `**Author**: ${parsedContent.data.author}\n\n`;
@@ -43,7 +52,16 @@ function addFrontmatterToContent(filePath) {
 
   newContent += parsedContent.content;
 
-  fs.writeFileSync(filePath, newContent, 'utf-8');
+  // Preserve the original frontmatter and append the new content
+  const updatedContent = matter.stringify(newContent, parsedContent.data, {
+    replacer: (key, value) => {
+      if (key === 'created' || key === 'last-call-deadline') {
+        return value.replace(/'/g, '');
+      }
+      return value;
+    },
+  });
+  fs.writeFileSync(filePath, updatedContent, 'utf-8');
 }
 
 const directoryPath = 'docs/pages/cips';
